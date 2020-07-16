843bfbc7ac889e855fc10a58d69538556b42a7d7
Joern-Marc Schmidt <joernmarc.schmidt@gmail.com>
2020-07-16 16:06:06 +0200

    Added Tests

--- src/main/java/org/xpdojo/bank/Account.java
+++ src/main/java/org/xpdojo/bank/Account.java
@@ -1,4 +1,25 @@
 package org.xpdojo.bank;
 
 public class Account {
+    int balance;
+
+    public Account (){
+        balance = 0;
+    }
+    public int balance() {
+        return balance;
+
+    }
+
+    public void deposit(int amount) {
+        balance += amount;
+    }
+
+    public boolean withdraw(int amount) {
+        if (amount > balance)
+            return false;
+        balance -= amount;
+        return true;
+    }
+
 }
--- src/test/java/org/xpdojo/bank/AccountTest.java
+++ src/test/java/org/xpdojo/bank/AccountTest.java
@@ -8,8 +8,39 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class AccountTest {
 
     @Test
-	@Disabled
-    public void depositAnAmountToIncreaseTheBalance() {
-        assertThat("your first test").isBlank();
+    public void newAccountShouldHaveZeroBalance() {
+        Account account = new Account();
+        assertThat(account.balance()).isEqualTo(0);
     }
+
+    @Test
+    public void depositAnAmountToIncreaseTheBalance(){
+        Account account = new Account();
+        account.deposit(10);
+        account.deposit(20);
+        assertThat(account.balance()).isEqualTo(30);
+    }
+
+    @Test
+    public void withdrawAnAmountHasBeenDeposited(){
+        Account account = new Account();
+        account.deposit(20);
+        account.withdraw(10);
+        assertThat(account.balance()).isEqualTo(10);
+    }
+
+    @Test
+    public void withdrawAnAmountFromEmptyAccountShouldNotWork(){
+        Account account = new Account();
+        assertThat(account.withdraw(10)).isFalse();
+    }
+
+    @Test
+    public void withdrawAnAmountHigherThanAvailable(){
+        Account account = new Account();
+        account.deposit(20);
+        assertThat(account.withdraw(30)).isFalse();
+    }
+
+
 }
